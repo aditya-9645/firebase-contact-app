ace09d71939dcae9e2cb34e9c84ac075277f5314
aditya-9645 <adityab201999@gmail.com>
2025-05-20 22:14:21 +0530

    Removed a bug

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -7,7 +7,7 @@ const Modal = ({isOpen, onClose, children}) => {
     <>
         {isOpen && (
             <div className="grid place-items-center absolute top-0 left-[-1px] backdrop-blur w-screen h-screen z-20">
-                <div className='relative  z-50 p-4 m-auto w-[80%] bg-white h-[345px] m-4 rounded-md' >
+                <div className='relative  z-50 p-4 m-auto w-[80%] bg-white h-[345px] my-4 rounded-md' >
                   <div className="flex justify-end">
                     <CgCloseR onClick={onClose} className=" text-xl self-end"/>
                   </div>
